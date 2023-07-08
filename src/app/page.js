265bf0341dcfb91642732a7172c38afea2b9c413
Kiriyako <doraemonboy158@gmail.com>
2023-07-08 15:15:50 +0530

    yes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 "use client"
 import { useState, useEffect } from "react"
-export default function Main(){
 
+export default function Main(){
+  const regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
   const [city, setCity] = useState('')
   const [submitCity, setSubmitCity] = useState('Kyoto')
   const [weatherData, setWeatherData] = useState(null)
   
-
   function finalCity(e){
     e.preventDefault();
    setSubmitCity(city)
@@ -27,17 +27,15 @@ export default function Main(){
     }
    }, [submitCity])
    if(weatherData && weatherData?.message == "city not found"){
-    return (<div id = "cnf"> City Not Found </div>)
+    return (<div className="text-4xl text-white bg-pink-400 rounded-2xl w-100% h-20 p-5" id = "cnf"> City Not Found </div>)
    }
-  const temp = Math.round(weatherData && weatherData.main.temp)
-  const feelstemp = Math.round(weatherData && weatherData.main.feels_like)
-  const epochTime = (weatherData && weatherData.dt)
+   const { main, weather, sys } = weatherData || {};
+   const temp = Math.round(main?.temp);
+   const feelstemp = Math.round(main?.feels_like);
+   const epochTime = weatherData?.dt;
+   const icon = weather?.[0]?.icon;
   const normalDate = new Date(epochTime * 1000);
-  const icon = (weatherData && weatherData.weather[0].icon)
-  const regionNames = new Intl.DisplayNames(
-    ['en'], {type: 'region'}
-  );
-  const countryId = weatherData?.sys?.country;
+  const countryId = sys?.country;
   const country = countryId ? regionNames.of(countryId) : '';
   const link = `https://openweathermap.org/img/wn/${icon}.png`
 
@@ -47,8 +45,13 @@ export default function Main(){
 
     <form onSubmit={finalCity}>  
       <div className="justify-center" id ="input">
-
-      <input className=" w-80 rounded-2xl border-2 p-2 text-xl bg-black/60" onChange={sinCity} value={city} placeholder="What's the weather like in..?" /> <button className="align-middle border-2 rounded-3xl text-xl h-10 w-10 p-2 bg-black/60 hover:bg-black/20" onClick={finalCity}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"></path></svg></button></div>
+      <input className=" w-80 rounded-2xl border-2 p-2 text-xl bg-black/60" onChange={sinCity} value={city} placeholder="What's the weather like in..?" /> <button className="align-middle border-2 rounded-3xl text-xl h-10 w-10 p-2 bg-black/60 hover:bg-black/20" onClick={finalCity}>
+  <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clipRule="evenodd"></path>
+    <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clipRule="evenodd"></path>
+  </svg>
+</button>
+</div>
     </form><br /> </div>
     <div>
 
